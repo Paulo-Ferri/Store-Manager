14b8b682c4ea584b59a752161491e6f17c16da4a
Paulo Afonso <pauloferrisouza@gmail.com>
2022-06-02 14:46:47 -0300

    feat: created productMiddleware for quantity and name checking

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productsService = require('../services/productsService');
+const productMiddlewares = require('../middlewares/productMiddlewares');
 
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.get('/:id', async (req, res) => {
   return res.status(status).json(...message);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', productMiddlewares.checkProductNameAndQuantity, async (req, res) => {
   const { name, quantity } = req.body;
   const { message, status, productRegistred } = await productsService.createProduct(name, quantity);
   if (productRegistred) return res.status(status).json(productRegistred);
